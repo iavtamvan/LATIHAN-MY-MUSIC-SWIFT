f6e8a1137ab9af985eba6062ccbc3655376542b0
Ade Fajr Ariav <ade.fajr.ariav@gmail.com>
2023-02-15 09:37:46 +0700

    first commit

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var stopBtn: UIButton!
+    @IBOutlet var playBtn: UIButton!
+    
+    @IBOutlet var musicLbl: UILabel!
+    private var player: AVAudioPlayer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: Memastikan bahwa url dari asset tidak null.
+            guard let url = Bundle.main.url(forResource: "astrid", withExtension: "mp3") else {
+              return
+            }
+                
+            do {
+              try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                    
+                musicLbl.text = "Astrid"
+              // MARK: Kode ini untuk iOS 11 ke atas.
+              player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                    
+              // MARK: Kode ini untuk iOS 10 ke bawah.
+              // player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3)
+                    
+            } catch let error {
+              print(error.localizedDescription)
+            }
+        
     }
 
+    @IBAction func playMusic(_ sender: Any) {
+        guard let audiuoPlayer = player else {return}
+        audiuoPlayer.play()
+        playBtn.isEnabled = false
+        
+        isPlaying(state: true)
+
+        
+        
+    }
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        guard let audiuoPlayer = player else {return}
+        audiuoPlayer.stop()
+        
+        isPlaying(state: false)
+        
+    }
+    
+    
+    private func isPlaying(state: Bool) {
+      stopBtn.isEnabled = state
+      playBtn.isEnabled = !state
+    }
 
 }
+
 
